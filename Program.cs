e36ce370ffbd1d4de2bc2e4b98a529cf62422366
Parjad Minooei <155120660+ParjadM@users.noreply.github.com>
2024-09-30 05:29:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is marks my professional career in C# 
+            //Also my First C# Code uploaded on github
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
